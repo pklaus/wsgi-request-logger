b67b81ad658b00ffc442ee5ff91c78443760ebff
Philipp Klaus <philipp.klaus@gmail.com>
2013-10-10 23:25:36 +0200

    Releasing v0.4.1 (containing bug fixes)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except (IOError, ImportError):
     LDESC = ''
 
 setup(name='wsgi-request-logger',
-      version = '0.4',
+      version = '0.4.1',
       description = 'Apache-like combined logging for WSGI Web Applications',
       long_description = LDESC,
       author = 'Philipp Klaus',
